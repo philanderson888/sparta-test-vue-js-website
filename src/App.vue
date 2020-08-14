d01322673563722018cc377180ff298fd08b7b90
philanderson888 <philanderson888@hotmail.com>
2020-08-14 15:08:36 +0100

    Updating Navbar Links

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,9 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link> |
+      <a href="https://www.bbc.co.uk">BBC</a> |
+      <router-link to="/about">Another Page</router-link> |
     </div>
     <router-view/>
   </div>
